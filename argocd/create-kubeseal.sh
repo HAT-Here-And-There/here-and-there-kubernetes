a67463733371df7cd79b961e45909a433b0db54e
junho100 <bemodesty306@naver.com>
2024-06-17 16:10:29 +0900

    Feat: refactor secrets in aws

--- argocd/create-kubeseal.sh
+++ argocd/create-kubeseal.sh
@@ -1,3 +1,3 @@
+cat chat.secret.helm.yaml | kubeseal --scope cluster-wide -oyaml > chat.sealed-secret.helm.yaml --controller-name=sealed-secrets
+cat tour.secret.helm.yaml | kubeseal --scope cluster-wide -oyaml > tour.sealed-secret.helm.yaml --controller-name=sealed-secrets
 cat user.secret.helm.yaml | kubeseal --scope cluster-wide -oyaml > user.sealed-secret.helm.yaml --controller-name=sealed-secrets
-
-cat secret.helm-aws.yaml | kubeseal --scope cluster-wide -oyaml > sealed-secret.helm-aws.yaml --controller-name=sealed-secrets
