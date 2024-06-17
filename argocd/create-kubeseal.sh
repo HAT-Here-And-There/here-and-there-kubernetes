a0b750392c1a4cb895ba2e5360af31835cf414b9
junho100 <bemodesty306@naver.com>
2024-06-17 14:02:11 +0900

    Feat: add prod secrets & command

--- argocd/create-kubeseal.sh
+++ argocd/create-kubeseal.sh
@@ -1,1 +1,3 @@
 cat secret.helm.yaml | kubeseal --scope cluster-wide -oyaml > sealed-secret.helm.yaml --controller-name=sealed-secrets
+
+cat secret.helm-aws.yaml | kubeseal --scope cluster-wide -oyaml > sealed-secret.helm-aws.yaml --controller-name=sealed-secrets
